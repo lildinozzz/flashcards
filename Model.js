de1450b49f771e0d8b0e2f5bbabf6aed2437c6a1
Артур <milkywaychik1601@gmail.com>
2024-02-09 14:21:30 +0300

    all done

--- Model.js
+++ Model.js
@@ -1,15 +1,21 @@
 const fs = require('fs').promises;
 
 class Model {
-  static async createQuestion() {
-    const files = await fs.readdir('./topics');
-    return files.map((file) => file.replace('.txt', ''));
-  }
-
-  static async readFile(fileName) {
-    const data = (await fs.readFile(`./topics/${fileName}_flashcard_data.txt`, 'utf-8'));
-    return data.split('\n');
+  constructor(question, answer) {
+    this.question = question;
+    this.answer = answer;
   }
 }
 
-Model.readFile('raccoon').then(console.log);
+async function getQuestion(fileName) {
+  const data = await fs.readFile(`./topics/${fileName}_flashcard_data.txt`, 'utf-8').then((elem) => elem.split('\n\n')); // array with 'Являются ли еноты травоядными, плотоядными или всеядными?\nвсеядными',
+  const newData = data.map((elem) => elem.split('\n')); // мэпаем на два элема в одном массиве
+  return newData.map((elem) => { // мэпаем по элементу и создаем обьект
+    const object = new Model(elem[0], elem[1]);
+    return object;
+  });
+}
+
+console.log(getQuestion('otter').then(console.log));
+
+module.exports = Model;
