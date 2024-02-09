e540bbef9e300f3c84ad00c49925d8342e241c68
Артур <milkywaychik1601@gmail.com>
2024-02-09 13:47:23 +0300

    done

--- Model.js
+++ Model.js
@@ -1,5 +1,15 @@
+const fs = require('fs').promises;
+
 class Model {
-    
+  static async createQuestion() {
+    const files = await fs.readdir('./topics');
+    return files.map((file) => file.replace('.txt', ''));
+  }
+
+  static async readFile(fileName) {
+    const data = (await fs.readFile(`./topics/${fileName}_flashcard_data.txt`, 'utf-8'));
+    return data.split('\n');
+  }
 }
 
-module.exports = Model;
+Model.readFile('raccoon').then(console.log);
